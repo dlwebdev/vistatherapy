36849f35bbe5bce0ebd30b8b1347fd86bd0b1f77
Daniel Weber <dw@thedanielweber.com>
2017-11-24 14:49:23 -0500

    Combined view-jobs with jobs. They should be same view

--- src/app/components/jobs/jobs.component.ts
+++ src/app/components/jobs/jobs.component.ts
@@ -10,15 +10,52 @@ import { JobService } from '../../services/job.service';
 })
 export class JobsComponent implements OnInit {
   jobs: Job[];
+  currentJobsToShow: Job[];
+  professions: any[];
+  search: {};
+  selectedProfession = 'None';
 
   constructor(private jobService: JobService) { }
 
   ngOnInit() {
+    this.professions = [
+      {id: 0, title: 'Bus Monitor'},
+      {id: 1, title: 'Occupational Therapist'},
+      {id: 2, title: 'Speech Language Pathologist'},
+      {id: 3, title: 'Speech Language Pathologist Assistant'}
+    ];
+
+    this.search = {
+      title: '',
+      employer: '',
+      location: ''
+    };
+
+    this.currentJobsToShow = this.jobs;
     this.getJobs();
+
+    console.log('Professions set: ', this.professions);
   }
 
   getJobs(): void {
     this.jobService.getJobs()
-    .subscribe(jobs => this.jobs = jobs);
+      .subscribe(jobs => { this.jobs = jobs; this.currentJobsToShow = jobs; });
   }
+
+  doSearch(): void {
+    console.log('Doing search.');
+    this.currentJobsToShow = this.jobs;
+
+    if (this.selectedProfession) {
+      console.log('Filtering....');
+      const pro = this.selectedProfession;
+      this.currentJobsToShow = this.currentJobsToShow.filter(function (job) {
+        console.log('Job: ', job);
+        console.log('Profession filter by: ', pro);
+
+        return job.profession === pro;
+      });
+    }
+  }
+  
 }
