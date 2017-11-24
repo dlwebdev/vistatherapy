15aa062dd49f4b00ce09ab736bfee2396217aaba
Your Name <you@example.com>
2017-11-24 13:27:44 -0500

    Added reactive side to top nav filter. Pulling in jobs.

--- src/app/components/view-jobs/view-jobs.component.ts
+++ src/app/components/view-jobs/view-jobs.component.ts
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-jobs.component.css']
 })
 export class ViewJobsComponent implements OnInit {
+  jobs: any[];
+  professions: any[];
+  search: {};
+  selectedProfession = -1;
 
   constructor() { }
 
   ngOnInit() {
+    this.professions = [
+      {id: 0, title: 'Bus Monitor'},
+      {id: 1, title: 'Occupational Therapist'},
+      {id: 2, title: 'Speech Language Pathologist'},
+      {id: 3, title: 'Speech Language Pathologist Assistant'}
+    ];
+
+    this.search = {
+      title: '',
+      employer: '',
+      location: ''
+    };
+
+    console.log('Professions set: ', this.professions);
   }
 
 }
